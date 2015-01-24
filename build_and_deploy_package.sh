0176535b22e212d78bc342de28e6c635fea76de9
James Muranga <jmured@gmail.com>
2015-01-24 15:26:28 +0300

    Making sure that the bower packages are built before the application is deployed

--- build_and_deploy_package.sh
+++ build_and_deploy_package.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 set -e
+bower install
 OUTPUT_FOLDER='builds'
 CURRENT_REVISION=`git rev-parse --short  HEAD`
 OUTPUT_FILE="$OUTPUT_FOLDER/$CURRENT_REVISION.tar.gz"
@@ -9,7 +10,7 @@ go get
 GOOS=linux GOARCH=amd64 go build -o $APP_NAME
 # docker run --rm -v "$(pwd)":/go/src/github.com/gophergala/abbita -w /go/src/github.com/gophergala/abbita  -e GOOS=linux -e GOARCH=amd64 golang:1.3 go get && go build -o $APP_NAME
 tar -zcf $OUTPUT_FILE $APP_NAME public
-rm -rf $APP_NAME
+rm -f $APP_NAME
 echo "Application saved to $OUTPUT_FILE"
 cp $OUTPUT_FILE builds/latest.tar.gz
 echo "Deploying app"
