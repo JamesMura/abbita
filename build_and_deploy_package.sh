fadc4e6e65e0d1699c6900bf23c5bb8bd938c32e
James Muranga <jmured@gmail.com>
2015-01-24 15:15:37 +0300

    Added deployment scripts

---
+++ build_and_deploy_package.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+set -e
+OUTPUT_FOLDER='builds'
+CURRENT_REVISION=`git rev-parse --short  HEAD`
+OUTPUT_FILE="$OUTPUT_FOLDER/$CURRENT_REVISION.tar.gz"
+echo "Building application"
+APP_NAME="abbita"
+go get 
+GOOS=linux GOARCH=amd64 go build -o $APP_NAME
+# docker run --rm -v "$(pwd)":/go/src/github.com/gophergala/abbita -w /go/src/github.com/gophergala/abbita  -e GOOS=linux -e GOARCH=amd64 golang:1.3 go get && go build -o $APP_NAME
+tar -zcf $OUTPUT_FILE $APP_NAME public
+rm -rf $APP_NAME
+echo "Application saved to $OUTPUT_FILE"
+cp $OUTPUT_FILE builds/latest.tar.gz
+echo "Deploying app"
+ansible-playbook -i provision/inv.ini provision/app.yml 
