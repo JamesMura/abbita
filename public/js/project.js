1a1b8e63eaaa7968119ad04d89133d73544b5baf
James Muranga <jmured@gmail.com>
2015-01-26 00:00:53 +0300

    Added route to create session

--- public/js/project.js
+++ public/js/project.js
@@ -1,18 +1,40 @@
 var application = angular
 .module('abbita', ['angularFileUpload', 'ui.router'])
-.controller('NewSessionController', ['$scope', 'FileUploader', function($scope, FileUploader) {
-	$scope.uploader = new FileUploader();
-	$scope.actions = ['Convert to wav', 'Convert to mp4'];
+.controller('NewSessionController', ['$scope', 'FileUploader','$state', function($scope, FileUploader, $state) {
+	var uploader = $scope.uploader = new FileUploader({url: '/api/sessions/new', alias: 'fileUpload'});
+	uploader.onSuccessItem = function(fileItem, response, status, headers) {
+		console.info('onSuccessItem', response);
+		$state.go('session',{id:response.id})
+	};
+	uploader.onErrorItem = function(fileItem, response, status, headers) {
+		console.info('onErrorItem', fileItem, response, status, headers);
+	};
+	
+	$scope.upload = function(item) {
+		item.formData.push({name: item.file.name})
+		item.upload();
+	}
+}]).controller('SessionController', ['$scope','$state','$http','$stateParams', function($scope, $state, $http, $stateParams) {
+	var promise = $http.get('/api/sessions/'+$stateParams.id);
+	promise.then(function(response) {
+		$scope.session = response.data;
+	})
+	
 }]);
 
 application.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
-    $urlRouterProvider.otherwise("/");
-    $stateProvider
-    .state('home', {
-        url: "/",
-        templateUrl: 'templates/home.html',
-        controller: 'NewSessionController'
-    })
+	$urlRouterProvider.otherwise("/");
+	$stateProvider
+	.state('home', {
+		url: "/",
+		templateUrl: 'templates/home.html',
+		controller: 'NewSessionController'
+	})
+	.state('session', {
+		url: "/session/:id",
+		templateUrl: 'templates/session.html',
+		controller: 'SessionController'
+	})
 }])	
 
 application.filter('bytes', function() {
@@ -20,7 +42,7 @@ application.filter('bytes', function() {
 		if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
 		if (typeof precision === 'undefined') precision = 1;
 		var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
-			number = Math.floor(Math.log(bytes) / Math.log(1024));
+		number = Math.floor(Math.log(bytes) / Math.log(1024));
 		return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
 	}
 });
--- transcoder/transcoder_suite_test.go
+++ transcoder/transcoder_suite_test.go
@@ -1,8 +1,6 @@
 package transcoder_test
 
 import (
-	"github.com/gophergala/abbita/transcoder"
-	"github.com/modocache/gory"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
@@ -11,14 +9,5 @@ import (
 
 func TestTranscoder(t *testing.T) {
 	RegisterFailHandler(Fail)
-	defineFactories()
 	RunSpecs(t, "Transcoder Suite")
-}
-
-func defineFactories() {
-	gory.Define("mp3", transcoder.File{},
-		func(factory gory.Factory) {
-			factory["Name"] = "Elly-Wamala-Ebinyumu-Ebyaffe.mp3"
-			factory["Type"] = "audio/mp3"
-		})
 }
